48619ca00b04e852872cbfddb00c273ac53d4a4a
Ramazan_Zyamilov <nash96@bk.ru>
2022-12-19 18:48:23 +0400

    сделал свайпер+табы+аккордион+бургер

--- js/sript.js
+++ js/sript.js
@@ -1,7 +1,12 @@
 let burger = document.querySelector(".burger");
 let menu = document.querySelector(".header__nav");
 let menuLink = document.querySelectorAll(".nav__link");
-let searchBtn = document.querySelector(".header__btn__serch")
+let searchBtn = document.querySelector(".header__btn__serch");
+let closeBtn = document.querySelector(".close-search");
+let findBtn = document.querySelector(".find-search");
+let tabsLink = document.querySelectorAll(".work-link");
+let tabsItem = document.querySelectorAll(".work-content-tabs");
+let tabsImg = document.querySelectorAll(".work-before");
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger--active');
@@ -20,4 +25,45 @@ menuLink.forEach(function(el) {
 searchBtn.addEventListener('click', () => {
   document.querySelector('.search').classList.toggle('search--active');
   searchBtn.classList.toggle('header__btn__serch--active');
+  closeBtn.classList.toggle('close-search--active');
+  findBtn.classList.toggle('find-search--active');
+});
+
+closeBtn.addEventListener('click', ()=> {
+  document.querySelector('.search').classList.remove('search--active');
+  findBtn.classList.remove('find-search--active');
+  closeBtn.classList.remove('close-search--active');
+});
+
+const swiper = new Swiper('.hero-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.hero-pagination',
+    clickable: true
+  },
+ });
+
+ new Accordion('.questions-list', {
+  elementClass: 'questions-item',
+  triggerClass: 'item-top',
+  panelClass: 'item-bottom',
+  activeClass: 'questions-item--active'
+});
+
+
+tabsLink.forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    const path = e.currentTarget.dataset.path;
+
+    tabsLink.forEach(function(btn){btn.classList.remove('work-link--active')});
+    e.currentTarget.classList.add('work-link--active');
+
+    tabsItem.forEach(function(el){el.classList.remove('work-content-tabs--active')});
+    document.querySelector(`[data-target="${path}"]`).classList.add('work-content-tabs--active');
+
+    tabsImg.forEach(function(el){el.classList.remove('work-before--active')});
+    document.querySelector(`[data-imgtarget="${path}"]`).classList.add('work-before--active');
+
+  });
 });
